7f7cd23088599ea7dc1933b0e1766481982b44a3
pavanmohinkar097 <pavanmohi097@gmail.com>
2023-05-11 12:49:13 +0530

    10-05-2023

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const AeteamCard = () => {
+  <div className="card">
+    <h2>Ae team</h2>
+    <h3> they are see you</h3>
+  </div>
+};
+const ZateamCard  =()=>{
+  <div className="card">
+        <h2>Za team</h2>
+        <h3> they are see you</h3>
+      </div>
+}
+
+export const App = () => {
+  return (
+    <div>
+      <AeteamCard />
+      <ZateamCard />
+      
+    </div>
+  );
+};
